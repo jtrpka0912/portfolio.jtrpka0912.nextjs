0c1f9ef835b7503aaa86bd664645dd9bb4f3fbd8
Jeremy Trpka <jtrpka0912@gmail.com>
2021-11-17 13:00:37 -0500

    Refactored the social links and made it more dynamic.

--- components/layout/footer/footer-social.tsx
+++ components/layout/footer/footer-social.tsx
@@ -1,5 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faTwitter, faGithub, faStackOverflow, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { 
+    faFacebook, 
+    faTwitter, 
+    faGithub, 
+    faStackOverflow, 
+    faLinkedin, 
+    IconDefinition 
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 /**
  * @function FooterSocial
@@ -8,6 +16,31 @@ import { faFacebook, faTwitter, faGithub, faStackOverflow, faLinkedin } from "@f
  * @returns { JSX }
  */
 const FooterSocial = () => {
+    /**
+     * @interface SocialMediaItem
+     * @description A social media item that includes url and icon component.
+     * @property { string } url - The URL hyperlink to the social media site or email
+     * @property { IconDefinition } icon - The fontawesome icon component
+     */
+    interface SocialMediaItem {
+        url: string,
+        icon: IconDefinition
+    }
+
+    /**
+     * @var socialMediaItems
+     * @type { SocialMediaItem[] }
+     * @description A list of all social media (and email) items.
+     */
+    const socialMediaItems: SocialMediaItem[] = [
+        { url: 'mailto:jtrpka0912@gmail.com', icon: faEnvelope },
+        { url: 'https://www.facebook.com/jeremy.trpka.77/', icon: faFacebook },
+        { url: 'https://twitter.com/jtrpka0912', icon: faTwitter },
+        { url: 'https://www.linkedin.com/in/jeremy-trpka/', icon: faLinkedin },
+        { url: 'https://github.com/jtrpka0912', icon: faGithub },
+        { url: 'https://stackoverflow.com/users/story/4008500', icon: faStackOverflow }
+    ];
+
     /**
      * @var listItemClasses
      * @type { String }
@@ -18,35 +51,15 @@ const FooterSocial = () => {
     return (
         <nav className="mb-4">
             <ul>
-                <li className={ listItemClasses }>
-                    <a href="https://www.facebook.com/jeremy.trpka.77/" target="_blank">
-                        <FontAwesomeIcon size="2x" icon={ faFacebook } />
-                    </a>
-                </li>
-
-                <li className={ listItemClasses }>
-                    <a href="https://twitter.com/jtrpka0912" target="_blank">
-                        <FontAwesomeIcon size="2x" icon={ faTwitter } />
-                    </a>
-                </li>
-                
-                <li className={ listItemClasses }>
-                    <a href="https://www.linkedin.com/in/jeremy-trpka/" target="_blank">
-                        <FontAwesomeIcon size="2x" icon={ faLinkedin } />
-                    </a>
-                </li>
-                
-                <li className={ listItemClasses }>
-                    <a href="https://github.com/jtrpka0912" target="_blank">
-                        <FontAwesomeIcon size="2x" icon={ faGithub } />
-                    </a>
-                </li>
-                
-                <li className={ listItemClasses }>
-                    <a href="https://stackoverflow.com/users/story/4008500" target="_blank">
-                        <FontAwesomeIcon size="2x" icon={ faStackOverflow } />
-                    </a>
-                </li>
+                { socialMediaItems.map(( item: SocialMediaItem, index: number ) => {
+                    return (
+                        <li className={ listItemClasses } key={ index }>
+                            <a href={ item.url } target="_blank">
+                                <FontAwesomeIcon size="2x" icon={ item.icon } />
+                            </a>
+                        </li>
+                    );
+                }) }
             </ul>
         </nav>
     );

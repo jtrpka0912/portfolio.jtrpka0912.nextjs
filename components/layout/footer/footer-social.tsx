882b6e8ffba47a999e505ab9fd8059ba65b85ff9
Jeremy Trpka <jtrpka0912@gmail.com>
2021-11-17 13:04:20 -0500

    Added titles to the social media links.

--- components/layout/footer/footer-social.tsx
+++ components/layout/footer/footer-social.tsx
@@ -19,11 +19,13 @@ const FooterSocial = () => {
     /**
      * @interface SocialMediaItem
      * @description A social media item that includes url and icon component.
-     * @property { string } url - The URL hyperlink to the social media site or email
-     * @property { IconDefinition } icon - The fontawesome icon component
+     * @property { string } url - The URL hyperlink to the social media site or email.
+     * @property { string } title - HTML popup to show where the link will go to.
+     * @property { IconDefinition } icon - The fontawesome icon component.
      */
     interface SocialMediaItem {
         url: string,
+        title: string
         icon: IconDefinition
     }
 
@@ -33,12 +35,12 @@ const FooterSocial = () => {
      * @description A list of all social media (and email) items.
      */
     const socialMediaItems: SocialMediaItem[] = [
-        { url: 'mailto:jtrpka0912@gmail.com', icon: faEnvelope },
-        { url: 'https://www.facebook.com/jeremy.trpka.77/', icon: faFacebook },
-        { url: 'https://twitter.com/jtrpka0912', icon: faTwitter },
-        { url: 'https://www.linkedin.com/in/jeremy-trpka/', icon: faLinkedin },
-        { url: 'https://github.com/jtrpka0912', icon: faGithub },
-        { url: 'https://stackoverflow.com/users/story/4008500', icon: faStackOverflow }
+        { url: 'mailto:jtrpka0912@gmail.com', title: 'Email', icon: faEnvelope },
+        { url: 'https://www.facebook.com/jeremy.trpka.77/', title: 'Facebook Profile', icon: faFacebook },
+        { url: 'https://twitter.com/jtrpka0912', title: 'Twitter Profile', icon: faTwitter },
+        { url: 'https://www.linkedin.com/in/jeremy-trpka/', title: 'LinkedIn Profile', icon: faLinkedin },
+        { url: 'https://github.com/jtrpka0912', title: 'GitHub Profile', icon: faGithub },
+        { url: 'https://stackoverflow.com/users/story/4008500', title: 'StackOverflow Developer Story', icon: faStackOverflow }
     ];
 
     /**
@@ -53,7 +55,7 @@ const FooterSocial = () => {
             <ul>
                 { socialMediaItems.map(( item: SocialMediaItem, index: number ) => {
                     return (
-                        <li className={ listItemClasses } key={ index }>
+                        <li className={ listItemClasses } title={ item.title } key={ index }>
                             <a href={ item.url } target="_blank">
                                 <FontAwesomeIcon size="2x" icon={ item.icon } />
                             </a>

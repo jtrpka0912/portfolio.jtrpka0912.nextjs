d8f6031cfc44b196cae67e8e4501358a800ab934
Jeremy Trpka <jtrpka0912@gmail.com>
2021-11-17 11:58:34 -0500

    Added a bulma badge to the footer that links to its official website.

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -1,3 +1,5 @@
+import FooterBulma from "./footer-bulma";
+
 /**
  * @function Footer
  * @summary Footer component
@@ -8,6 +10,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="container py-6 has-background-primary has-text-centered">
+                <FooterBulma />
             </div>
         </footer>
     );

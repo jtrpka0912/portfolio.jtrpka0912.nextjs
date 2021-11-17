67e0b9dfbf45be4e4e33677400849eb3f71a455a
Jeremy Trpka <jtrpka0912@gmail.com>
2021-11-17 13:52:19 -0500

    Added some styling to layout and footer.

--- components/layout/footer/footer-social-item.tsx
+++ components/layout/footer/footer-social-item.tsx
@@ -15,7 +15,6 @@ interface FooterSocialItemProps {
  * @function FooterSocialItem
  * @summary Footer Social Item component
  * @description A footer social list item that is displayed with an icon and links to a profile or email
- * @todo Adjust the horizontal padding for small screens.
  * @param { FooterSocialItemProps } props 
  * @returns { JSX }
  */
@@ -23,7 +22,7 @@ const FooterSocialItem = (props: FooterSocialItemProps) => {
     const { item } = props;
 
     return (
-        <li className="is-inline-block px-5" title={ item.title }>
+        <li className="footer-social-item" title={ item.title }>
             <a href={ item.url } target="_blank">
                 <FontAwesomeIcon size="2x" icon={ item.icon } />
             </a>

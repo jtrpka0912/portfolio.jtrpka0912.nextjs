5d0692927aae89fa20abe1d7051b7c71bc0e74b8
Jeremy Trpka <jtrpka0912@gmail.com>
2021-11-17 12:11:23 -0500

    Started the footer social media component.

---
+++ components/layout/footer/footer-social.tsx
@@ -0,0 +1,17 @@
+/**
+ * @function FooterSocial
+ * @summary Footer social media component
+ * @description A list of social media links to my profiles.
+ * @returns { JSX }
+ */
+const FooterSocial = () => {
+    return (
+        <nav>
+            <ul>
+                
+            </ul>
+        </nav>
+    );
+};
+
+export default FooterSocial;

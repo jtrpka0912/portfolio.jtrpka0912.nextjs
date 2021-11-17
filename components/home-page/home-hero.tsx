3223c62189785833c8902c113b78358848c31d45
Jeremy Trpka <jtrpka0912@gmail.com>
2021-11-17 14:05:49 -0500

    Created a home hero component.

---
+++ components/home-page/home-hero.tsx
@@ -0,0 +1,13 @@
+/**
+ * @function HomeHero
+ * @summary Home Page Hero component
+ * @description A big hero banner for the home page
+ * @returns { JSX }
+ */
+const HomeHero = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default HomeHero;

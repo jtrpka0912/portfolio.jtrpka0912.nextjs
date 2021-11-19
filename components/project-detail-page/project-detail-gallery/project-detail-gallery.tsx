760ba0f1007c38ce200aa4838a6765a48e91d810
Jeremy Trpka <jtrpka0912@gmail.com>
2021-11-19 10:52:38 -0500

    Going to start gallery section for project detail page.

---
+++ components/project-detail-page/project-detail-gallery/project-detail-gallery.tsx
@@ -0,0 +1,16 @@
+/**
+ * @function ProjectDetailGallery
+ * @summary Project detail gallery component
+ * @description A tabular section to showcase images of the project in different sizes.
+ * @author J. Trpka
+ * @returns { JSX }
+ */
+const ProjectDetailGallery = () => {
+    return (
+        <section className="section">
+
+        </section>
+    );
+};
+
+export default ProjectDetailGallery;

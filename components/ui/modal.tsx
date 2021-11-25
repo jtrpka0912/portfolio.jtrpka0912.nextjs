141faf8188423ec76a21c0e9a87b2b710daf6ed2
Jeremy Trpka <jtrpka0912@gmail.com>
2021-11-25 06:39:06 -0500

    Created a skeletal modal for preparing the image modal.

---
+++ components/ui/modal.tsx
@@ -0,0 +1,14 @@
+/**
+ * @function Modal
+ * @summary Modal component
+ * @description A basic modal layout for multiple purposes
+ * @author J. Trpka
+ * @returns { JSX }
+ */
+const Modal = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default Modal;

b1af0fa33778431575169009be2d773500d5e826
Jeremy Trpka <jtrpka0912@gmail.com>
2021-11-30 22:02:51 -0500

    Might possibly be finished with project gallery component.

--- cypress/integration/project-detail-page/gallery.spec.ts
+++ cypress/integration/project-detail-page/gallery.spec.ts
@@ -61,5 +61,41 @@ describe('Count the gallery items', () => {
     });
 });
 
+describe('Interacting with gallery items', () => {
+    beforeEach(() => {
+        // Go to test project page
+        cy.visit('localhost:3000/project/test');
+    });
+
+    it('Click a tablet gallery item', () => {
+        // Action
+        cy.get('.gallery__panel .thumbnail')
+            .click();
+
+        // Assert
+        cy.get('.gallery__panel .thumbnail')
+            .eq(1)
+            .should('not.exist');
+
+        cy.get('.modal')
+            .should('have.class', 'is-active');
+    });
+
+    it('Click a mobile gallery item', () => {
+        // Action
+        cy.get('.gallery li')
+            .contains('Mobile')
+            .click();
+
+        cy.get('.gallery__panel .thumbnail')
+            .eq(1)
+            .click();
+
+        // Assert
+        cy.get('.modal')
+            .should('have.class', 'is-active');
+    });
+});
+
 // Not sure why this is necessary. Remove once you need to import something.
 export {};

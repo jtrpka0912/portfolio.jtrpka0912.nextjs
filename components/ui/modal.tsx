7a43a8990e4c2d04942e905cab8b352c1471c000
Jeremy Trpka <jtrpka0912@gmail.com>
2021-12-01 17:18:46 -0500

    Removed some TODOs.

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -23,8 +23,6 @@ interface ModalProps {
  * @summary Modal component
  * @description A basic modal layout for multiple purposes
  * @author J. Trpka
- * @todo Need to check if the active status will still work once state management is implemented.
- * @todo https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/dialog_role
  * @returns { JSX }
  */
 const Modal = (props: ModalProps) => {

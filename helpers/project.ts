50d142825ed8f83f5457d8b6a2efe592a969ea01
Jeremy Trpka <jtrpka0912@gmail.com>
2021-12-01 20:37:38 -0500

    Wired up retrieving all projects to the projects page.

--- helpers/project.ts
+++ helpers/project.ts
@@ -1,10 +1,41 @@
 import { Project } from "../models/project";
-import { getSinglePost } from "./markdown";
+import { getAllPosts, getSinglePost } from "./markdown";
 
+/**
+ * @var { string } category
+ * @description The folder name that contains the project posts markdown files
+ * @author J. Trpka
+ */
 const category: string = 'project';
 
+/**
+ * @var { string[] }
+ * @description A list of slugs to exclude when retrieving all project posts.
+ * @author J. Trpka
+ */
+const exclude: string[] = ['test'];
+
+/**
+ * @function getSingleProject
+ * @description Retrieve a single project.
+ * @author J. Trpka
+ * @param { string } postId 
+ * @returns { Project }
+ */
 export const getSingleProject = (postId: string): Project => {
     return getSinglePost<Project>(postId, category);
-}
+};
 
-// TODO: When coding to retrieve all projects. Make sure to exclude the test project.
+/**
+ * @function getAllProjects
+ * @description Retrieve all (non-excluded) projects
+ * @author J. Trpka
+ * @returns { Project[] }
+ */
+export const getAllProjects = (): Project[] => {
+    const allProjects = getAllPosts<Project>(category);
+
+    return allProjects.filter((project: Project) => {
+        return !exclude.includes(project.slug);
+    });
+};
--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -1,7 +1,39 @@
-const ProjectsPage = () => {
+import { GetStaticProps } from "next";
+
+import { getAllProjects } from "../../helpers/project";
+
+import { Project } from "../../models/project";
+
+/**
+ * @interface ProjectsPageProps
+ * @summary Project page props
+ * @author J. Trpka
+ * @property { Project[] } projects
+ */
+interface ProjectsPageProps {
+    projects: Project[]
+}
+
+const ProjectsPage = (props: ProjectsPageProps) => {
+    console.info('Projects', props.projects);
+    
     return (
         <div>Projects Page</div>
     );
 };
+
+interface ProjectsStaticProps {
+    projects: Project[]
+}
+
+export const getStaticProps:GetStaticProps<ProjectsStaticProps> = async() => {
+    const projects: Project[] = getAllProjects();
+
+    return {
+        props: {
+            projects
+        }
+    };
+}
 
 export default ProjectsPage;

5c6b44405d5a67841c3948a672f574c53c303b04
Jeremy Trpka <jtrpka0912@gmail.com>
2021-12-06 21:17:22 -0500

    Added convert repo url to owner and repo tuple function.

---
+++ __tests__/api/github.test.ts
@@ -0,0 +1,33 @@
+import { convertRepoURLToOwnerAndRepo } from '../../api/github';
+
+describe('convertRepoURLToOwnerAndRepo()', () => {
+    test('Get owner and repo from Github repo URL', () => {
+        // Arrange
+        const results = convertRepoURLToOwnerAndRepo('https://github.com/octocat/hello-world');
+
+        // Assert
+        expect(results).toStrictEqual(['octocat', 'hello-world']);
+    });
+
+    test('Get owner and repo from Github repo URL with www sub-domain', () => {
+        // Arrange
+        const results = convertRepoURLToOwnerAndRepo('https://www.github.com/octocat/hello-world');
+
+        // Assert
+        expect(results).toStrictEqual(['octocat', 'hello-world']);
+    });
+
+    test('Thrown error with obvious non-Github repo URL', () => {
+        // Assert
+        expect(() => {
+            convertRepoURLToOwnerAndRepo('https://www.duckduckgo.com');
+        }).toThrow('Invalid Github URL');
+    });
+
+    test('Thrown error with missing Github repo endpoint', () => {
+        // Assert
+        expect(() => {
+            convertRepoURLToOwnerAndRepo('https://www.github.com/octocat');
+        }).toThrow('Invalid Github URL')
+    });
+});
---
+++ api/github.ts
@@ -0,0 +1,35 @@
+/**
+ * @function getRepo
+ * @summary Get a Github Repo
+ * @description Take the URL of the Github repo, and break it down to retrieve data from Github's API.
+ * @author J. Trpka
+ * @param { string } repoUrl
+ */
+export const getRepo = (repoUrl: string) => {
+    // wait for something
+}
+
+/**
+ * @function convertRepoURLToOwnerAndRepo
+ * @summary Convert repo url to owner and repo
+ * @description Take a valid Github repo URL and retrieve the owner and repo values
+ * @author J. Trpka
+ * @param { string } repoUrl 
+ * @example https://www.github.com/octocat/hello-world
+ * @returns { [string, string] }
+ */
+export const convertRepoURLToOwnerAndRepo = (repoUrl: string): [string, string] => {
+    const repoUrlBreakdown = repoUrl.split('/');
+
+    // First check if the breakdown is successful
+    if(repoUrlBreakdown[0] === 'https:' && // Nowadays this should be a https website
+        repoUrlBreakdown[1] === '' && // Should be nothing. https:/{ this is why its empty}/
+        (repoUrlBreakdown[2] === 'github.com' || repoUrlBreakdown[2] === 'www.github.com') && // Either with(out) the www. sub-domain
+        (repoUrlBreakdown[3] && repoUrlBreakdown[4]) && // There needs to be values in the last two
+        repoUrlBreakdown.length === 5 // Finally, no more than five array elements
+    ) {
+        return [repoUrlBreakdown[3], repoUrlBreakdown[4]];
+    } else {
+        throw new Error('Invalid Github URL');
+    }
+}

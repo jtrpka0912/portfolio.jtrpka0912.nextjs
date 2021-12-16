84fb8fcb86b85f80371a331b1ad5ea756d633ff2
Jeremy Trpka <jtrpka0912@gmail.com>
2021-12-15 20:48:07 -0500

    Updated tests to reflect proper technology data.

--- __tests__/helpers/technology.test.ts
+++ __tests__/helpers/technology.test.ts
@@ -10,32 +10,33 @@ describe('getAllTechnologies()', () => {
             .mockImplementation(() => 
                 Promise.resolve({
                     ok: true,
-                    json: () => Promise.resolve({
-                        reactjs: {
+                    json: () => Promise.resolve([
+                        {
+                            slug: 'reactjs',
                             title: 'React',
                             url: '/path/to/home-page',
                             logo: '/path/to/image'
                         },
-                        typescript: {
+                        {
+                            slug: 'typescript',
                             title: 'TypeScript',
                             url: '/path/to/home',
                             logo: '/path/to/image'
                         },
-                        nextjs: {
+                        {
+                            slug: 'nextjs',
                             title: 'NextJS',
                             url: '/path/to/url',
                             logo: '/path/to/image'
                         }
-                    })
+                    ])
                 } as Response),
             );
 
         const technologies: Technology[] = await getAllTechnologies();
 
         // Assert
-        expect(technologies).toHaveProperty('reactjs');
-        expect(technologies).toHaveProperty('typescript');
-        expect(technologies).toHaveProperty('nextjs');
+        expect(technologies).toHaveLength(3);
     });
 
     test('Retrieving an empty list', async () => {
@@ -44,14 +45,14 @@ describe('getAllTechnologies()', () => {
             .mockImplementation(() => 
                 Promise.resolve({
                     ok: true,
-                    json: () => Promise.resolve({})
+                    json: () => Promise.resolve([])
                 } as Response),
             );
 
         const technologies: Technology[] = await getAllTechnologies();
 
         // Assert
-        expect(technologies).toMatchObject({});
+        expect(technologies).toHaveLength(0);
     });
 
     test('Handle an error', async () => {
@@ -60,7 +61,7 @@ describe('getAllTechnologies()', () => {
             .mockImplementation(() => 
                 Promise.resolve({
                     ok: false,
-                    json: () => Promise.resolve({})
+                    json: () => Promise.resolve([])
                 } as Response),
             );
 

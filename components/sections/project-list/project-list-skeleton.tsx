db5b26c6f436d4b8431eac0c7e55f353e233aecf
Jeremy Trpka <jtrpka0912@gmail.com>
2022-01-08 17:03:52 -0500

    Project list skeleton.

---
+++ components/sections/project-list/project-list-skeleton.tsx
@@ -0,0 +1,14 @@
+/**
+ * @function ProjectListSkeleton
+ * @summary Project list skeleton component
+ * @description An astract version of this component for page loading.
+ * @author J. Trpka
+ * @returns { JSX }
+ */
+const ProjectListSkeleton = () => {
+    return (
+        <section></section>
+    );
+};
+
+export default ProjectListSkeleton;

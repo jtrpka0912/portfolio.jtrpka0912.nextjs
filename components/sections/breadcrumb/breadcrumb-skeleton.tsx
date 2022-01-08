01b943369868346231272127a104dbd45785c667
Jeremy Trpka <jtrpka0912@gmail.com>
2022-01-08 11:32:01 -0500

    Breadcrumb skeleton initial skeleton.

---
+++ components/sections/breadcrumb/breadcrumb-skeleton.tsx
@@ -0,0 +1,14 @@
+/**
+ * @function BreadcrumbSkeleton
+ * @summary Breadcrumb skeleton
+ * @description Show an abstraction of the breadcrumb when loading page data.
+ * @author J. Trpka
+ * @returns { JSX }
+ */
+const BreadcrumbSkeleton = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default BreadcrumbSkeleton;

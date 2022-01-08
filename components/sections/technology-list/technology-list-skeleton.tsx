dff50e6f62fe478edb1d56bbfbb544dc875798d8
Jeremy Trpka <jtrpka0912@gmail.com>
2022-01-08 15:45:14 -0500

    Tech list skeleton.

---
+++ components/sections/technology-list/technology-list-skeleton.tsx
@@ -0,0 +1,14 @@
+/**
+ * @function TechnologyListSkeleton
+ * @summary Technology list skeleton component
+ * @description An astract version of this component for page loading.
+ * @author J. Trpka
+ * @returns { JSX }
+ */
+const TechnologyListSkeleton = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default TechnologyListSkeleton;

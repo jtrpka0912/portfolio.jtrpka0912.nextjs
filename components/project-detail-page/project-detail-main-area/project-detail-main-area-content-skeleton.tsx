f31cb4bae7768298e02cc946c554a8a722268e0c
Jeremy Trpka <jtrpka0912@gmail.com>
2022-01-08 20:30:39 -0500

    Main area content skeleton.

---
+++ components/project-detail-page/project-detail-main-area/project-detail-main-area-content-skeleton.tsx
@@ -0,0 +1,16 @@
+import BaseSkeleton from "../../ui/base-skeleton";
+
+/**
+ * @function ProjectDetailMainAreaContentSkeleton
+ * @summary Project detail main area content skeleton component
+ * @description An abstract version of this component for page loading.
+ * @author J. Trpka
+ * @returns { JSX }
+ */
+const ProjectDetailMainAreaContentSkeleton = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default ProjectDetailMainAreaContentSkeleton;

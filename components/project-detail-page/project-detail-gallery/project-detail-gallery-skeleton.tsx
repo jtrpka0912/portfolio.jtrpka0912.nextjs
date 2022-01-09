286d9062313cf16f1d6c06728895338641cd16fb
Jeremy Trpka <jtrpka0912@gmail.com>
2022-01-08 20:56:03 -0500

    Project detail gallery skeleton.

---
+++ components/project-detail-page/project-detail-gallery/project-detail-gallery-skeleton.tsx
@@ -0,0 +1,20 @@
+import BaseSkeleton from "../../ui/base-skeleton";
+
+/**
+ * @function ProjectDetailGallerySkeleton
+ * @summary Project detail gallery skeleton component
+ * @description An abstract version of this component for page loading.
+ * @author J. Trpka
+ * @returns { JSX }
+ */
+const ProjectDetailGallerySkeleton = () => {
+    return (
+        <section className="section">
+            <div className="container">
+
+            </div>
+        </section>
+    );
+};
+
+export default ProjectDetailGallerySkeleton;

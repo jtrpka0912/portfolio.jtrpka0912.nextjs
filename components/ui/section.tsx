ad328b31e0bfb704c4898f9c89b3e973bda520d7
Jeremy Trpka <jtrpka0912@gmail.com>
2022-01-22 10:27:38 -0500

    Component skeletal structure.

---
+++ components/ui/section.tsx
@@ -0,0 +1,29 @@
+/**
+ * @interface SectionProps
+ * @summary Section component props
+ * @author J. Trpka
+ * @property { string } title
+ * @property { string } backgroundColor
+ * @property { React.ReactNode } children
+ */
+interface SectionProps {
+    title?: string;
+    backgroundColor?: string;
+    children: React.ReactNode;
+}
+
+/**
+ * @function Section
+ * @summary Section component
+ * @description A simple section component that can provide an optional title prior to content and background color. It is advised if there are special needs for the typical section component then do not use this component and just stick with .section > .container elements.
+ * @author J. Trpka
+ * @param { SectionProps } props
+ * @return { JSX }
+ */
+const Section = (props: SectionProps) => {
+    return (
+        <section></section>
+    );
+};
+
+export default Section;

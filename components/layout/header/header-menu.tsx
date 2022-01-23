6ebf37a01f2cd0dbd2c466c2bb105bf3fbdc27ab
Jeremy Trpka <jtrpka0912@gmail.com>
2022-01-23 18:56:03 -0500

    Closing menu when links are clicked.

--- components/layout/header/header-menu.tsx
+++ components/layout/header/header-menu.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { UiContext } from '../../context/ui';
 
 /**
@@ -7,16 +7,30 @@ import { UiContext } from '../../context/ui';
  * @summary Header Menu component
  * @description Header menu with links for the application.
  * @author J. Trpka
- * @todo: Add is-active class to show the menu on mobile
  * @returns { JSX }
  */
 const HeaderMenu = () => {
     const uiContext = useContext(UiContext);
+
+    // When the links are clicked then close the menu.
+    useEffect(() => {
+        const linkElement = document.querySelector('#header-navigation .navbar-item');
+
+        if(linkElement) {
+            linkElement.addEventListener('click', e => {
+                uiContext.setHeaderMobileMenu();
+            })
+        }
+    }, [])
     
     return (
         <div id="header-navigation" 
         className={ `navbar-menu${ uiContext.isHeaderMobileMenuOpen ? ' is-active' : '' }` }>
             <div className="navbar-end">
+                <Link href="/project">
+                    <a className="navbar-item">Projects</a>
+                </Link>
+
                 <Link href="/contact">
                     <a className="navbar-item">Contact</a>
                 </Link>

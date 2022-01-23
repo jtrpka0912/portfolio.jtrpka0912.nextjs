62153bc336f1ada6b72ccdb9656e3c3e4e5ca062
Jeremy Trpka <jtrpka0912@gmail.com>
2022-01-23 18:40:24 -0500

    Navbar burger is wired up.

--- components/layout/header/header-burger.tsx
+++ components/layout/header/header-burger.tsx
@@ -1,18 +1,28 @@
+// React
+import { useContext } from "react";
+
+// Context
+import { UiContext } from "../../context/ui";
 
 /**
  * @function HeaderBurger
  * @summary Header Burger component
  * @description: The header button that expands the menu on mobile devices.
  * @todo Add the is-active class when the menu does expand.
- * @todo Change the aria expanded when it is open.
  * @returns JSX
  */
 const HeaderBurger = () => {
+    const uiContext = useContext(UiContext);
+
     return (
         <a role="button" 
-            className="navbar-burger"
+            className={ 
+                `navbar-burger
+                ${ uiContext.isHeaderMobileMenuOpen ? ' is-active' : null }` 
+            }
             aria-label="menu"
-            aria-expanded="false"
+            aria-expanded={ uiContext.isHeaderMobileMenuOpen ? 'true' : 'false' }
+            onClick={ uiContext.setHeaderMobileMenu }
         >
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>

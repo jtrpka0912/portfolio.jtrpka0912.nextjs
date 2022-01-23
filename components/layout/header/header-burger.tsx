6542eca2408f677a5fbc8115b9fb473acd921e5d
Jeremy Trpka <jtrpka0912@gmail.com>
2022-01-23 18:47:21 -0500

    Wired up the header navigation.

--- components/layout/header/header-burger.tsx
+++ components/layout/header/header-burger.tsx
@@ -8,20 +8,18 @@ import { UiContext } from "../../context/ui";
  * @function HeaderBurger
  * @summary Header Burger component
  * @description: The header button that expands the menu on mobile devices.
- * @todo Add the is-active class when the menu does expand.
- * @returns JSX
+ * @author J. Trpka
+ * @returns { JSX }
  */
 const HeaderBurger = () => {
     const uiContext = useContext(UiContext);
 
     return (
         <a role="button" 
-            className={ 
-                `navbar-burger
-                ${ uiContext.isHeaderMobileMenuOpen ? ' is-active' : null }` 
-            }
+            className={ `navbar-burger${ uiContext.isHeaderMobileMenuOpen ? ' is-active' : '' }` }
             aria-label="menu"
             aria-expanded={ uiContext.isHeaderMobileMenuOpen ? 'true' : 'false' }
+            data-target="header-navigation"
             onClick={ uiContext.setHeaderMobileMenu }
         >
             <span aria-hidden="true"></span>
--- components/layout/header/header-menu.tsx
+++ components/layout/header/header-menu.tsx
@@ -1,15 +1,21 @@
 import Link from 'next/link';
+import { useContext } from 'react';
+import { UiContext } from '../../context/ui';
 
 /**
  * @function HeaderMenu
  * @summary Header Menu component
  * @description Header menu with links for the application.
+ * @author J. Trpka
  * @todo: Add is-active class to show the menu on mobile
- * @returns JSX
+ * @returns { JSX }
  */
 const HeaderMenu = () => {
+    const uiContext = useContext(UiContext);
+    
     return (
-        <div className="navbar-menu">
+        <div id="header-navigation" 
+        className={ `navbar-menu${ uiContext.isHeaderMobileMenuOpen ? ' is-active' : '' }` }>
             <div className="navbar-end">
                 <Link href="/contact">
                     <a className="navbar-item">Contact</a>

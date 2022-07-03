4d8ec3c21194db8b347310bfe8abfa8e239bee78
J. Trpka <jtrpka0912@gmail.com>
2022-07-03 15:14:49 -0400

    Renamed IResume and all of its interfaces.

--- models/IResume.ts
+++ models/IResume.ts
@@ -1,54 +1,54 @@
 import { IPost } from './IPost';
 
 /**
- * @interface Resume
+ * @interface IResume
  * @extends IPost
  * @description A single job experience resume post
  * @author J. Trpka
  * @property { string } employer Name of the company or employer
- * @property { ResumeJobTitle } jobtitle The job title that was given from the start and/or ended
- * @property { ResumeDate } date The first and/or last day working with the company/employer
- * @property { ResumeLocation } address The location of the company/employer
- * @property { ResumeContact } contact Different methods of contacting the company or employer
+ * @property { IResumeJobTitle } jobtitle The job title that was given from the start and/or ended
+ * @property { IResumeDate } date The first and/or last day working with the company/employer
+ * @property { IResumeLocation } address The location of the company/employer
+ * @property { IResumeContact } contact Different methods of contacting the company or employer
  * @property { string[] } technology Technologies that were used throughout the entire job experience
  * @property { boolean } featured List this job experience on the featured list
  */
-export interface Resume extends IPost {
+export interface IResume extends IPost {
     employer: string,
-    jobtitle: ResumeJobTitle,
-    date: ResumeDate,
-    address?: ResumeLocation,
-    contact: ResumeContact,
+    jobtitle: IResumeJobTitle,
+    date: IResumeDate,
+    address?: IResumeLocation,
+    contact: IResumeContact,
     technology: string[]
     featured: boolean,
 }
 
 /**
- * @interface ResumeJobTitle
+ * @interface IResumeJobTitle
  * @description The job title that was given at the started and (if applicable) that was given when ended
  * @author J. Trpka
  * @property { string } started
  * @property { string } ended
  */
-interface ResumeJobTitle {
+interface IResumeJobTitle {
     started?: string,
     ended?: string
 }
 
 /**
- * @interface ResumeDate
+ * @interface IResumeDate
  * @description When the job experience started and (if applicable) ended
  * @author J. Trpka
  * @property { string } started
  * @property { string } ended
  */
-interface ResumeDate {
+interface IResumeDate {
     started?: string,
     ended?: string
 }
 
 /**
- * @interface ResumeLocation
+ * @interface IResumeLocation
  * @description The location of the office
  * @author J. Trpka
  * @property { string } street
@@ -58,7 +58,7 @@ interface ResumeDate {
  * @property { string } postal
  * @property { string } country
  */
-interface ResumeLocation {
+interface IResumeLocation {
     street: string,
     suite?: string,
     city: string,
@@ -68,13 +68,13 @@ interface ResumeLocation {
 }
 
 /**
- * @interface ResumeContact
+ * @interface IResumeContact
  * @description The different methods to contact the job experience company/employer
  * @author J. Trpka
  * @property { string } phone
  * @property { string } email
  */
-interface ResumeContact {
+interface IResumeContact {
     phone?: string,
     email?: string
 }

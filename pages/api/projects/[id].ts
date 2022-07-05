f82c7253b9be098d74e28b2fffa0f0db7f49afea
J. Trpka <jtrpka0912@gmail.com>
2022-07-05 09:45:12 -0400

    Created a placeholder api call for retrieving a single project.

---
+++ pages/api/projects/[id].ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({
+    "hello-world": "foo-bar"
+  });
+}
+
+export default handler;

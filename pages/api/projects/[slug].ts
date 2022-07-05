345e7c2b15331c5a89010a747c5841fd059f5575
J. Trpka <jtrpka0912@gmail.com>
2022-07-05 10:57:31 -0400

    Retrieved a Contentful entry for API.

--- pages/api/projects/[slug].ts
+++ pages/api/projects/[slug].ts
@@ -1,11 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { retrieveProjectById, retrieveProjectBySlug } from "../../../api/contentful/projects";
+import { Entry } from "contentful";
+
+import { retrieveProjectBySlug } from "../../../api/contentful/projects";
+import { IContentfulProject } from "../../../models/api/contentful/content-types/IContentfulProject";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  retrieveProjectBySlug('portfolio');
-  // const response = await retrieveProjectById('4fVBG83m9kAGaQu2XNUEhl');
-  // console.info(response);
-
+  /**
+   * @constant { Promise<Entry<IContentfulProject>> } contentfulProjectResponse
+   * @summary Contentful project response
+   * @description Response from the Contentful API to retrieve a project by slug
+   * @author J. Trpka
+   * @note Because a single value of query can either be string or string[]; need to check if slug is an array or not
+   */
+  const contentfulProjectResponse: Entry<IContentfulProject> = 
+    await retrieveProjectBySlug(Array.isArray(req.query.slug) ? req.query.slug[0] : req.query.slug);
+  
   res.status(200).json({
     "hello-world": "foo-bar"
   });

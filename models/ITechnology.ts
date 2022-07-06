7af8cb9f9687cd4a6300a118adddffe4df55dd2e
J. Trpka <jtrpka0912@gmail.com>
2022-07-06 06:49:38 -0400

    Added ID to ITechnology.

--- models/ITechnology.ts
+++ models/ITechnology.ts
@@ -4,12 +4,14 @@ import { IImage } from "./IImage"
  * @interface Technology
  * @description A major tool that was used for a project or job experience.
  * @author J. Trpka
+ * @property { string } id - Contentful ID
  * @property { string } name
  * @property { string } url - The home page
  * @property { IImage } logo
  */
 export interface ITechnology {
-    name: string
-    url: string
-    logo?: IImage
+    id: string;
+    name: string;
+    url: string;
+    logo?: IImage;
 }

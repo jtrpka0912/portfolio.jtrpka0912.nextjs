59dc569b9e9be08369392d93e069630bc9e7b3bb
J. Trpka <jtrpka0912@gmail.com>
2022-07-15 19:25:51 -0400

    NPM packages link does not show, but the sidebar element still shows.

--- __tests__/components/project-detail-page/sidebar/project-detail-package/project-detail-package.test.tsx
+++ __tests__/components/project-detail-page/sidebar/project-detail-package/project-detail-package.test.tsx
@@ -11,6 +11,20 @@ describe('Render the component', () => {
         expect(element).not.toBeInTheDocument();
     });
 
+    test('Do not render the content if there are no packages from an empty IProject', () => {
+        // Arrange
+        render(<ProjectDetailPackage package={{
+            npm: [],
+            go: [],
+            maven: []
+        }} />);
+
+        const element = document.querySelector('.project-detail-package');
+
+        // Assert
+        expect(element).not.toBeInTheDocument();
+    });
+
     test('Render if there are any packages.', () => {
         render(<ProjectDetailPackage package={{
             maven: ['spring-boot-starter-test']
--- components/project-detail-page/sidebar/project-detail-package/project-detail-package.tsx
+++ components/project-detail-page/sidebar/project-detail-package/project-detail-package.tsx
@@ -15,13 +15,32 @@ interface ProjectDetailPackageProps {
  * @function ProjectDetailPackage
  * @summary Project detail sidebar package component
  * @description A sidebar component that list out all other miscellaneous packages used for the project.
+ * @author J. Trpka
  * @param { ProjectDetailPackageProps } props 
  * @returns { JSX }
  */
 const ProjectDetailPackage = (props: ProjectDetailPackageProps) => {
     const { package: packages } = props;
 
-    if(!packages) {
+    /**
+     * @function isEmptyPackages
+     * @summary Any packages contained in IProjectPackage
+     * @description Check if any of the type of IProjectPackage has any packages. If empty then return true.
+     * @author J. Trpka
+     * @todo Would prefer to loop through IProjectPackage instead of manually add each package type.
+     * @returns { boolean }
+     */
+    const isEmptyPackages = (arrayOfPackages?: Array<any>): boolean => {
+        if(!arrayOfPackages || arrayOfPackages.length === 0) return true;
+        return false;
+    }
+
+    if(!packages || (
+        isEmptyPackages(packages.npm) 
+        && isEmptyPackages(packages.maven) 
+        && isEmptyPackages(packages.go)
+        && isEmptyPackages(packages.nuget)
+    )) {
         return null;
     }
 

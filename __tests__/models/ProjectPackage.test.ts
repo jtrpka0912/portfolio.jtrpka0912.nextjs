7d3a37da3ec156662ebee18e1623b5b870af31fc
J. Trpka <jtrpka0912@gmail.com>
2022-07-15 20:11:33 -0400

    Added some more tests for the ProjectPackage class.

--- __tests__/models/ProjectPackage.test.ts
+++ __tests__/models/ProjectPackage.test.ts
@@ -16,4 +16,68 @@ describe('Constructor', () => {
     expect(projectPackageObject.npm).toHaveLength(0);
     expect(projectPackageObject.nuget).toHaveLength(0);
   });
+
+  test('Convert from an empty object', () => {
+    // Arrange
+    const projectPackageObject: ProjectPackage = new ProjectPackage({});
+
+    // Assert
+    expect(projectPackageObject.go).toBeUndefined();
+    expect(projectPackageObject.maven).toBeUndefined();
+    expect(projectPackageObject.npm).toBeUndefined();
+    expect(projectPackageObject.nuget).toBeUndefined();
+  });
+
+  test('Convert from undefined', () => {
+    // Arrange
+    const projectPackageObject: ProjectPackage = new ProjectPackage(undefined);
+
+    // Assert
+    expect(projectPackageObject.go).toBeUndefined();
+    expect(projectPackageObject.maven).toBeUndefined();
+    expect(projectPackageObject.npm).toBeUndefined();
+    expect(projectPackageObject.nuget).toBeUndefined();
+  });
 });
+
+describe('isEmpty()', () => {
+  test('Return false when there are packages', () => {
+    // Arrange
+    const projectPackageObject: ProjectPackage = new ProjectPackage({
+      npm: [],
+      maven: ['spring-boot', 'spring-mvc'],
+      go: ['websocket'],
+      nuget: []
+    });
+
+    // Assert
+    expect(projectPackageObject.isEmpty()).toBeFalsy();
+  })
+
+  test('Return true when there are no packages', () => {
+    // Arrange
+    const projectPackageObject: ProjectPackage = new ProjectPackage({
+      npm: [],
+      go: []
+    });
+
+    // Assert
+    expect(projectPackageObject.isEmpty()).toBeTruthy();
+  })
+
+  test('Return true when it is just an empty void', () => {
+    // Arrange
+    const projectPackageObject: ProjectPackage = new ProjectPackage({});
+
+    // Assert
+    expect(projectPackageObject.isEmpty()).toBeTruthy();
+  })
+
+  test('Return true when it is just undefined', () => {
+    // Arrange
+    const projectPackageObject: ProjectPackage = new ProjectPackage(undefined);
+
+    // Assert
+    expect(projectPackageObject.isEmpty()).toBeTruthy();
+  })
+})
--- models/ProjectPackage.ts
+++ models/ProjectPackage.ts
@@ -44,11 +44,44 @@ class ProjectPackage implements IProjectPackage {
    * @author J. Trpka
    * @param { IProjectPackage } projectPackage 
    */
-  constructor(projectPackage: IProjectPackage) {
+  constructor(projectPackage?: IProjectPackage) {
+    if(!projectPackage) return;
+
     this.npm = projectPackage.npm;
     this.go = projectPackage.go;
     this.maven = projectPackage.maven;
     this.nuget = projectPackage.nuget;
+  }
+
+  /**
+   * @function isEmpty
+   * @summary Is packages empty
+   * @description Check if there are no packages set, if so return true
+   * @author J. Trpka
+   * @todo Would rather if the properties were looped instead of manually added
+   * @returns { boolean }
+   */
+  public isEmpty = (): boolean => {
+    if(this.isPackagesEmpty(this.npm)
+      && this.isPackagesEmpty(this.go)
+      && this.isPackagesEmpty(this.maven)
+      && this.isPackagesEmpty(this.nuget)) return true;
+
+    return false;
+  }
+
+  /**
+   * @function isPackagesEmpty
+   * @summary Is type of packages empty
+   * @description Is a set of packages empty; if so return true.
+   * @author J. Trpka
+   * @param { any[] } arrayOfPackages - There is no point checking what type of array; just need to be identified as an array
+   * @returns { boolean }
+   */
+  private isPackagesEmpty = (arrayOfPackages?: Array<any>): boolean => {
+    if(!arrayOfPackages || arrayOfPackages.length === 0) return true;
+    
+    return false;
   }
 }
 

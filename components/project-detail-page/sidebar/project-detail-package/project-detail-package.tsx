f6066d622b6e5a84d95d03b65fb29968e7f11849
J. Trpka <jtrpka0912@gmail.com>
2022-07-15 20:19:30 -0400

    Reverted the functionality from the project detail package component to use the class.

--- components/project-detail-page/sidebar/project-detail-package/project-detail-package.tsx
+++ components/project-detail-page/sidebar/project-detail-package/project-detail-package.tsx
@@ -1,4 +1,5 @@
 import { IProjectPackage } from "../../../../models/IProject";
+import ProjectPackage from "../../../../models/ProjectPackage";
 import ProjectDetailNpm from "./project-detail-npm";
 
 /**
@@ -16,48 +17,31 @@ interface ProjectDetailPackageProps {
  * @summary Project detail sidebar package component
  * @description A sidebar component that list out all other miscellaneous packages used for the project.
  * @author J. Trpka
+ * @todo Change the name of the package property, from IProject, to packages
  * @param { ProjectDetailPackageProps } props 
  * @returns { JSX }
  */
 const ProjectDetailPackage = (props: ProjectDetailPackageProps) => {
     const { package: packages } = props;
 
-    /**
-     * @function isEmptyPackages
-     * @summary Any packages contained in IProjectPackage
-     * @description Check if any of the type of IProjectPackage has any packages. If empty then return true.
-     * @author J. Trpka
-     * @todo Would prefer to loop through IProjectPackage instead of manually add each package type.
-     * @returns { boolean }
-     */
-    const isEmptyPackages = (arrayOfPackages?: Array<any>): boolean => {
-        if(!arrayOfPackages || arrayOfPackages.length === 0) return true;
-        return false;
-    }
+    const projectPackageObject: ProjectPackage = new ProjectPackage(packages);
 
-    if(!packages || (
-        isEmptyPackages(packages.npm) 
-        && isEmptyPackages(packages.maven) 
-        && isEmptyPackages(packages.go)
-        && isEmptyPackages(packages.nuget)
-    )) {
-        return null;
-    }
+    if(!packages || projectPackageObject.isEmpty()) return null;
 
     return (
         <div className="project-detail-package" 
             role="note"
             aria-label="Project Packages"
         >
-            { packages.npm && (
-                <ProjectDetailNpm packages={ packages.npm } />
+            { projectPackageObject.npm && (
+                <ProjectDetailNpm packages={ projectPackageObject.npm } />
             ) }
 
-            { packages.maven && (
+            { projectPackageObject.maven && (
                 <p>Maven repositories coming soon</p>
             ) }
 
-            { packages.go && (
+            { projectPackageObject.go && (
                 <p>Go packages coming soon</p>
             ) }
         </div>

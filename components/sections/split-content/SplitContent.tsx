2b570d4bc4256d813628d17fe64d717a7a479611
J. Trpka <jtrpka0912@gmail.com>
2022-08-05 20:12:11 -0400

    Created component.

---
+++ components/sections/split-content/SplitContent.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Section from "../../ui/section"
+
+/**
+ * @function SplitContent
+ * @summary Split text and image section component.
+ * @description A content section that shows textual content and an image.
+ * @author J. Trpka
+ * @returns { React.FC }
+ */
+const SplitContent = () => {
+  return (
+    <Section>
+      <div>Hello World</div>
+    </Section>
+  );
+};
+
+export default SplitContent;

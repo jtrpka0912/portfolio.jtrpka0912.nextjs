271ea9c44ebee9f6ff9e1065d2a450d2b3ac7e3c
jtrpka0912 <jtrpka0912@gmail.com>
2025-11-12 11:24:29 -0500

    Created a simple main header for the root layout.

---
+++ src/components/layout/main-header.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+import MainHeader from './main-header';
+
+const meta = {
+    component: MainHeader,
+    title: 'Portfolio/Layout/Main Header'
+} satisfies Meta<typeof MainHeader>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const PrimaryMainHeader: Story = {};
---
+++ src/components/layout/main-header.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react"
+
+const MainHeader = () => {
+    return (
+        <Box as="header">
+            <Heading as="h1">Hello, World!</Heading>
+        </Box>
+    );
+};
+
+export default MainHeader;

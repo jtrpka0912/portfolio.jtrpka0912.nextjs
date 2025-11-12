041e4e635d280c3e3016d7feaad94ca3348fc0ac
jtrpka0912 <jtrpka0912@gmail.com>
2025-11-12 11:41:10 -0500

    Applied some styling.

--- src/components/layout/main-header.tsx
+++ src/components/layout/main-header.tsx
@@ -1,8 +1,15 @@
 import { Box, Heading } from "@chakra-ui/react"
 
+/**
+ * @function MainHeader
+ * @summary Main Header Component
+ * @description The main header for the NextJS root layout
+ * @author J. Trpka <jtrpka0912@gmail.com>
+ * @returns { React.ReactNode }
+ */
 const MainHeader = () => {
     return (
-        <Box as="header">
+        <Box as="header" backgroundColor="teal.muted" p="2">
             <Heading as="h1">Hello, World!</Heading>
         </Box>
     );

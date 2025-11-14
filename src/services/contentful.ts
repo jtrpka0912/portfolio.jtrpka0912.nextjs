37f3402fc2c94a00c3e6a7d056f9d3712ba6870d
jtrpka0912 <jtrpka0912@gmail.com>
2025-11-14 15:27:05 -0500

    Created a contentful service object.

---
+++ src/services/contentful.ts
@@ -0,0 +1,40 @@
+import * as contentful from 'contentful';
+import { env } from 'process';
+
+/**
+ * @function retrieveSpaceID
+ * @summary Retrieve Contentful Space ID from environment variable
+ * @description Attempt to retrieve the Contentful Space ID, via an environment variable, but if none then throw an error.
+ * @author J.Trpka<jtrpka0912@gmail.com>
+ * @throws {Error} Throw an error to halt the service.
+ * @returns {string} Contentful Space ID
+ */
+const retrieveSpaceID = (): string => {
+  if (!env.CONTENTFUL_SPACE_ID) throw Error('Space ID not set');
+
+  return env.CONTENTFUL_SPACE_ID;
+};
+
+/**
+ * @function retrieveAccessToken
+ * @summary Retrieve API keys from environment variables
+ * @description Attempt to retrieve Contentful API keys, via environment variables, but if any are not set then throw an error.
+ * @author J. Trpka<jtrpka0912@gmail.com>
+ * @throws {Error} Throw an error to halt the service.
+ * @returns {string} The API key depending on current Node environment
+ */
+const retrieveAccessToken = (): string => {
+  if (env.NODE_ENV === 'production') {
+    if (!env.CONTENTFUL_CONTENT_DELIVERY_API) throw Error('Content Delivery API not set');
+    return env.CONTENTFUL_CONTENT_DELIVERY_API;
+  } else {
+    if (!env.CONTENTFUL_CONTENT_PREVIEW_API) throw Error('Content Preview API not set');
+    return env.CONTENTFUL_CONTENT_PREVIEW_API;
+  }
+};
+
+export default contentful.createClient({
+  space: retrieveSpaceID(),
+  accessToken: retrieveAccessToken(),
+  environment: env.CONTENTFUL_ENVIRONMENT ?? 'master',
+});

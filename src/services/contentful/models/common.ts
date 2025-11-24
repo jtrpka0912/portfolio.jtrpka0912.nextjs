2ebbae192adab2ff0380836917e1116d13181154
jtrpka0912 <jtrpka0912@gmail.com>
2025-11-24 11:23:43 -0500

    Sigh...

--- src/services/contentful/models/common.ts
+++ src/services/contentful/models/common.ts
@@ -1,5 +1,93 @@
 /**
  * @type
+ * @name EntryItem
+ * @summary Entry Item
+ * @description The entries that were retrieved from the response.
+ * @author J. Trpka<jtrpka0912@gmail.com>
+ * @param {ItemMetadata} metadata - Not sure what this does
+ * @param {ItemSystem} sys - System information for the item
+ * @param {object} fields - The fields for the entry
+ */
+export type EntryItem = {
+  metadata: ItemMetadata;
+  sys: ItemSystem;
+
+  // Irrevelant to know at the moment until my system programmatically finds out the content type it is.
+  fields: object;
+};
+
+/**
+ * @type
+ * @name ItemMetadata
+ * @author J. Trpka<jtrpka0912@gmail.com>
+ * @note Likely not to be used.
+ * @param {string[]} tags - Not sure what this contains
+ * @param {string[]} concepts - Not sure what this contains
+ */
+type ItemMetadata = {
+  tags: string[];
+  concepts: string[];
+};
+
+/**
+ * @type
+ * @name ItemSystem
+ * @summary Item System Data
+ * @description System information of the entry item including entry ID.
+ * @author J. Trpka<jtrpka0912@gmail.com>
+ * @param {string} type - The type of item (likely an Entry)
+ * @param {string} id - Entry Identifier
+ * @param {object} contentType - The type of item it is.
+ * @param {number} revision - Not sure what this is
+ * @param {string} createdAt - When the entry was first created
+ * @param {string} updatedAt - When the entry was last updated
+ * @param {string} publishedAt - When the entry was last published
+ * @param {string} firstPublishedAt - When the entry was first published
+ * @param {number} publishedVersion - Not sure what this is
+ * @param {string} locale - Language used for the entry
+ */
+type ItemSystem = {
+  // ... not everything is included
+  type: string;
+  id: string;
+  contentType: ContentType;
+  revision: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  firstPublishedAt: string;
+  publishedVersion: number;
+  locale: string;
+};
+
+/**
+ * @type
+ * @name ContentType
+ * @summary Content type data
+ * @description Information of the content type
+ * @author J. Trpka<jtrpka0912@gmail.com>
+ * @param {LinkSystem} sys - Content type system data
+ */
+type ContentType = {
+  sys: LinkSystem;
+};
+
+/**
+ * @type
+ * @name IncludedEntries
+ * @summary Included entries
+ * @description Items that were included due to being linked to other entries.
+ * @author J. Trpka<jtrpka0912@gmail.com>
+ * @param {EntryItem[]} Entry - Entry linked items
+ * @param {object[]} Asset - Asset (images) linked items
+ */
+export type IncludedEntries = {
+  Entry: EntryItem[];
+  Asset: object[];
+};
+
+/**
+ * @type
  * @name Link
  * @summary Link data
  * @description Information of the link (Entry or Asset)
@@ -12,16 +100,16 @@ export type Link = {
 
 /**
  * @type
- * @name LinkSystem
- * @summary Link system data
- * @description Information about the link
+ * @name System
+ * @summary System data
+ * @description System data about the item
  * @author J. Trpka<jtrpka0912@gmail.com>
- * @param {string} type - The type of item (likely a Link)
- * @param {string} linkType - The type of link
- * @param {string} id - The entry's identifier
+ * @param {string} type - The type of item
+ * @param {string|undefined} linkType - The type of link
+ * @param {string|undefined} id - The entry's identifier
  */
-type LinkSystem = {
-  type: 'Link'; // NOTE: Can this be anything else?
-  linkType: 'Link' | 'Asset';
-  id: string;
+type System = {
+  type: string;
+  linkType?: string;
+  id?: string;
 };
--- src/services/contentful/models/entries-response.ts
+++ src/services/contentful/models/entries-response.ts
@@ -1,3 +1,5 @@
+import { EntryItem, IncludedEntries } from './common';
+
 /**
  * @type
  * @name EntriesResponse
@@ -8,69 +10,13 @@
  * @param {number} total - Total number of entries (pagination)
  * @param {number} skip - Pagination skip index (pagination)
  * @param {number} limit - The amount of entries per page (pagination)
- * @param {T[]} items - The items (Entries) array
+ * @param {EntryItem[]} items - The items (Entries) array
+ * @param {IncludedEntries} includes - Items that were included like linked Entry and Assets
  */
 export type EntriesResponse<T> = {
   total: number;
   skip: number;
   limit: number;
-  items: EntriesItem<T>[];
-};
-
-/**
- * @type
- * @name EntriesItem
- * @summary Entries Individual Item
- * @description The entries that were retrieved from the response.
- * @author J. Trpka<jtrpka0912@gmail.com>
- * @param {ItemMetadata} metadata - Not sure what this does
- * @param {ItemSystem} sys - System information for the item
- * @param {T} fields - The fields for the entry
- */
-type EntriesItem<T> = {
-  metadata: ItemMetadata;
-  sys: ItemSystem;
-  fields: T;
-};
-
-/**
- * @type
- * @name ItemMetadata
- * @author J. Trpka<jtrpka0912@gmail.com>
- * @note Likely not to be used.
- * @param {string[]} tags - Not sure what this contains
- * @param {string[]} concepts - Not sure what this contains
- */
-type ItemMetadata = {
-  tags: string[];
-  concepts: string[];
-};
-
-/**
- * @type
- * @name ItemSystem
- * @summary Item System Data
- * @description System information of the entry item including entry ID.
- * @author J. Trpka<jtrpka0912@gmail.com>
- * @param {string} type - The type of item (likely an Entry)
- * @param {string} id - Entry Identifier
- * @param {number} revision - Not sure what this is
- * @param {string} createdAt - When the entry was first created
- * @param {string} updatedAt - When the entry was last updated
- * @param {string} publishedAt - When the entry was last published
- * @param {string} firstPublishedAt - When the entry was first published
- * @param {number} publishedVersion - Not sure what this is
- * @param {string} locale - Language used for the entry
- */
-type ItemSystem = {
-  // ... not everything is included
-  type: string;
-  id: string;
-  revision: number;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  firstPublishedAt: string;
-  publishedVersion: number;
-  locale: string;
+  items: EntryItem[];
+  includes: IncludedEntries;
 };
